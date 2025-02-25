38d606a84ccec3ddeb0bc5a3880ff509a79a8873
kaspinning <kaspining@gmail.com>
2025-02-26 08:34:27 +0900

    カーソル速度_低速時_0.3,0.5追加

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -639,9 +639,9 @@ static int pmw3610_report_data(const struct device *dev) {
 
     float speed_multiplier = 1.0; //速度の倍率
     if (movement_size > 60) {
-        speed_multiplier = 1.0;
+        speed_multiplier = 0.3;
     }else if (movement_size > 30) {
-        speed_multiplier = 1.0;
+        speed_multiplier = 0.5;
     }else if (movement_size > 5) {
         speed_multiplier = 1.0;
     }else if (movement_size > 4) {
